5b19e775c3017479c1fac45f530b932a9d77d378
Abhishek Hulage <abhihulage14@gmail.com>
2024-06-11 16:20:05 +0530

    Added map function for note

--- keeper/src/App.js
+++ keeper/src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import Navbar from './Navbar/navbar';
 import Footer from './Footer/footer';
 import Note from './Note/note';
+import notes from './notes'
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Note />
+
+      {notes.map((note) => (
+          <Note 
+            key={note.id}
+            title={note.title}
+            content={note.content}
+          />
+      ))}
+
       <Footer />
     </div>
   );
